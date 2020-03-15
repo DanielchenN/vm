3e5af4dc0fecb226d9a4bf5a29ca8453d6361b8c
chenguo <guochen@shein.com>
2020-03-15 14:50:51 +0800

    feat: add child process

--- execute.js
+++ execute.js
@@ -3,30 +3,32 @@ const fs = require('fs')
 
 let result = ''
 
-const code = 'var a =1; a'
-const code2 = `this.constructor.constructor('return this.process')();
-process.mainModule.require('child_process').execSync('cat /etc/passwd').toString()`
-
 const genRunner = function() {
   return function run(code) {
     const result = new Function(`return eval(${JSON.stringify(code)})`)
-    // send('end', result);
     return result()
   }
 }
 
 
-const run = function() {
+const run = function(code) {
   const context = vm.createContext(Object.create(null))
   const runner = vm.runInContext(`(${genRunner.toString()})()`, context)
-  // const runner2 = vm.runInContext(code, context)
+  // const runner2 = vm.runInContext(code2, context)
   // console.log('runner2', runner2)
   try {
-    const a = runner(code)
+    const res = runner(code)
+    process.send(res)
+    process.exit(0)
   } catch(e) {
-    result += e.name + e.message
+    result += e.name + ' ' + e.message
+    process.send(JSON.stringify(result))
+    process.exit(0)
   }
 }
 
 
-run()
+process.on('message', (m) => {
+  console.log('子进程收到消息', m);
+  run(m)
+});
--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-const { EventEmitter } = require('event')
 const { spawn } = require('child_process')
+const path = require('path')
 
-class vm extends EventEmitter {
 
+const code = 'var a = {"b": 123}; a'
+const code2 = `this.constructor.constructor('return this.process')();
+process.mainModule.require('child_process').execSync('cat /etc/passwd').toString()`
+
+class VM  {
+  constructor() {
+    this.options = {
+      timeout: 500,
+      node:    'node',
+      executor:  path.join(__dirname, 'execute.js')
+    }
+    this.error = null
+    this.result = null
+  }
+
+  run(code, cb) {
+    this.child = spawn(this.options.node, [this.options.executor], {stdio: ['inherit', 'inherit', 'inherit', 'ipc']})
+    this.child.send(code)
+    this.child.on('message', (chunk) => {
+      console.log('父进程chunk', chunk)
+      this.result = chunk
+    })
+    this.child.on('exit', () => {
+      console.log('exit')
+      cb(this.error, this.result)
+    })
+
+    this.child.timer = setTimeout(() => {
+      this.error = JSON.stringify({ result: 'TimeoutError', console: [] })
+      this.child.kill('SIGKILL')
+    }, this.options.timeout)
+  }
 }
+
+const a = new VM()
+a.run(code)
+
